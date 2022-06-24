5a2e644784c4a536d8d29f343832a96991ac10a7
Brian Koech <brnkoech@gmail.com>
2022-06-24 08:13:43 +0300

    Add shared component styles to one 'shared' folder

--- src/shared/styles.tsx
+++ src/shared/styles.tsx
@@ -5,15 +5,19 @@ export const Container = styled.div`
   flex-direction: row;
   flex-wrap: wrap;
   justify-content: space-around;
+
+  &::after {
+    content: '';
+    flex: auto;
+  }
 `;
 
 export const Wrapper = styled.div`
   display: flex;
-  align-items: 'center';
+  align-items: center;
   background: #fff;
   box-shadow: 0 2px 1px -1px #0003, 0 1px 1px #00000024, 0 1px 3px #0000001f;
 `;
-
 
 export const Icon = styled.img`
   padding: 5px;
@@ -23,3 +27,7 @@ export const Title = styled.h2`
   margin-left: 5px;
 `;
 
+export const Img = styled.img`
+  height: ${({ height }) => `${height}px` ?? 'auto'};
+  max-height: calc(100vh - 200px);
+`;
